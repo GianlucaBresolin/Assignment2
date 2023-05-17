f23633d462cccb5b76997b1e159e26245ae8a1e6
GianlucaBresolin <117901582+GianlucaBresolin@users.noreply.github.com>
2023-05-17 14:38:17 +0200

    close #9
    Implementazione IntegerToRoman

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,10 +1,42 @@
+////////////////////////////////////////////////////////////////////
+// GIANLUCA BRESOLIN 2034316
+// GIOVANNI MENON 2034301
+////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
 public class IntegerToRoman {
+            
+    private static final int[] VALUES = { 1000, 900, 500, 400, 
+                                         100, 90, 50, 40, 10, 9, 
+                                         5, 4, 1 };
 
-    public static String convert(int number){
-        // TODO
-    return null;
-    }
+    private static final String[] SYMBOLS = { "M", "CM", "D", "CD", 
+                                             "C", "XC", "L", "XL", 
+                                             "X", "IX", "V", "IV", 
+                                             "I" };
+    
+    public static String convert(int number)
+        throws NumberUnderZeroException, 
+            NumberOverOnethousandException {
         
+        if (number < 0) {
+            throw new NumberUnderZeroException();
+        }
+        if (number > 1000) {
+            throw new NumberOverOnethousandException();
+        }
+    
+        StringBuilder roman = new StringBuilder();
+        int remaining = number;
+    
+        for (int i = 0; i < VALUES.length; i++) {
+            while (remaining >= VALUES[i]) {
+                roman.append(SYMBOLS[i]);
+                remaining -= VALUES[i];
+            }
+        }
+    
+        return roman.toString();
+    }
+   
 }
